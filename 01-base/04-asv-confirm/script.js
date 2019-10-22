7ff9333732e941b667abfbf0f50aebf0e1fcc50d
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-10-22 14:28:10 +0200

    Exe 1 - 04 ok

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -9,9 +9,30 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
-    let asking = () => {
-        
+(() => {    
+    const ask_sex = () => {
+        let sex = "";
+        sex = prompt("Quel est votre sexe ? (Homme/Femme/Autre)");
+        sex = sex.toUpperCase();
+        if( sex != "HOMME" && sex != "FEMME" && sex !="AUTRE"){
+            alert("Veuillez indiquer Homme ou Femme ou Autre")
+            ask_sex();
+        }
+        return sex;
     }
+    
+    const asking = () => {
+        let age = prompt("Quel âge avez-vous ?");
+        let sex = ask_sex();
+        let city = prompt("De quelle ville venez-vous ?");
+        let confirmation = confirm("Confirmez vous les données suivantes?\n" + age + " ans\n" + sex + "\n" + city)
+        if (confirmation == TRUE){
+            alert("Bienvenue !");
+        }
+        else{
+            alert("Veuiller fournir vos données exactes");
+            asking();
+        }
+    }
+    asking();
 })();
