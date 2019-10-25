741106076994f9bc02a2df51f6ea15fadbc427f7
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-10-25 16:40:16 +0200

    exe 5.9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -11,4 +11,24 @@
 
 (() => {
     // your code here
+    const stat = () => {
+        let arr_random = [];
+        for(let i=1;i<=10;i++){
+            arr_random[i-1] = Math.floor(Math.random() * 101);
+            document.getElementById(`n-${i}`).innerHTML = arr_random[i-1];
+        }
+        let min = Math.min(...arr_random);
+        let max = Math.max(...arr_random);
+        let sum = arr_random.reduce((a,b) => a + b);
+        let average = sum / arr_random.length;
+        
+        document.getElementById(`min`).innerHTML = min;
+        document.getElementById("max").innerHTML = max;
+        document.getElementById("sum").innerHTML = sum;
+        document.getElementById('average').innerHTML = average;
+    }
+
+    document.getElementById("run").addEventListener("click", stat);
+
+
 })();
