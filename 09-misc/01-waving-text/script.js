39ae0d5db33bd68b62cd438d9c325c7581094082
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-10-29 21:01:46 +0100

    9.1 wave

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -10,5 +10,26 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    
+    const daWave = () => {
+        let bigString = document.getElementById("target").innerHTML;
+        bigString = [...bigString];
+        console.log(bigString);
+        bigStringLength = bigString.length;
+        let waveCount = 0;
+        let arrClassWaveSize = [ "small", "medium", "large", "x-large", "xx-large"]
+        for(let i = 0; i < bigStringLength; i++){
+            if(bigString[i] != " "){
+                waveCount += 1;
+                waveCount > 4 ? waveCount = 0 : 0;
+                bigString[i] = `<span class="wave${waveCount}" style="font-size: ${arrClassWaveSize[waveCount]}">${bigString[i]}</span>`;
+            }
+        }
+        bigString = bigString.join('');
+        document.getElementById("target").innerHTML = bigString;
+    }
+    
+    document.getElementById("run").addEventListener("click", daWave);
+
+
 })();
